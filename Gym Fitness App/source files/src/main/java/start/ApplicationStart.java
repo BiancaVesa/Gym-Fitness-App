66c5ac611ad856765ed353907b99128548d7b503
BiancaVesa <bianca.vesa55@yahoo.com>
2021-02-22 12:02:41 +0200

    Final Version Gym Fitness App

---
+++ Gym Fitness App/source files/src/main/java/start/ApplicationStart.java
@@ -0,0 +1,22 @@
+package start;
+
+import controller.LoginController;
+import entity.*;
+import repository.*;
+import service.ClientService;
+import service.ScheduleService;
+import view.LoginView;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+public class ApplicationStart {
+
+    public static void main(String[] args) {
+
+        LoginController.run();
+        new LoginView();
+
+    }
+}
